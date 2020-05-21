d907750c8ec4256fa00afd82bd2c42c591c712f6
Lenic <Lenic@live.cn>
2020-05-21 14:15:48 +0800

    project init.
    Signed-off-by: Lenic <Lenic@live.cn>

---
+++ config/webpack.config.js
@@ -0,0 +1,46 @@
+const path = require('path');
+const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+function getPath(dir) {
+  return path.join(process.cwd(), dir);
+}
+
+module.exports = {
+  mode: 'development',
+  devtool: 'source-map',
+  entry: getPath('src/index.tsx'),
+  output: {
+    publicPath: '/',
+    filename: 'js/[name].js',
+    path: getPath('dist'),
+    chunkFilename: 'js/chunks/[id].js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        exclude: /node_modules/,
+        loader: 'ts-loader'
+      },
+      {
+        test: /\.(css|less)$/,
+        use: [
+          { loader: 'style-loader' },
+          { loader: 'css-loader', options: { importLoaders: 2 } },
+          { loader: 'less-loader' }
+        ]
+      }
+    ]
+  },
+  resolve: {
+    extensions: ['.js', '.ts', '.tsx']
+  },
+  plugins: [
+    new webpack.NamedChunksPlugin(),
+    new HtmlWebpackPlugin({
+      title: 'TS-DEMO',
+      template: getPath('config/index.html')
+    })
+  ]
+};
---
+++ src/app.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+import './app.less';
+
+export const App = () => {
+  return <div className="container">Hello</div>;
+};
